8e3b9c7a655b4e0504f0b27775ef8fa77f339ae6
renancvitor <renan.vitor.cm@gmail.com>
2025-06-12 16:57:12 -0300

    Comentário importante em ApiApplication sobre os próximos passos

--- src/main/java/hub/forum/api/ApiApplication.java
+++ src/main/java/hub/forum/api/ApiApplication.java
@@ -9,5 +9,5 @@ public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
-// Curso 02, tópico 02, vídeo 02
+// Curso 02, tópico 02, vídeo 03
 }
