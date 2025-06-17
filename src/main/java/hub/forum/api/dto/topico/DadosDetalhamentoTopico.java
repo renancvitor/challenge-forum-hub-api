dc839e444207b67a4d3782b5729920cfcf26d10a
renancvitor <renan.vitor.cm@gmail.com>
2025-06-17 11:08:27 -0300

    Refatora e limpa código

--- src/main/java/hub/forum/api/dto/topico/DadosDetalhamentoTopico.java
+++ src/main/java/hub/forum/api/dto/topico/DadosDetalhamentoTopico.java
@@ -1,28 +1,28 @@
-package hub.forum.api.dto.topico;
-
-import hub.forum.api.domain.topico.StatusTopico;
-import hub.forum.api.domain.topico.Topico;
-
-import java.time.LocalDateTime;
-
-public record DadosDetalhamentoTopico(
-        Long id,
-        String titulo,
-        String mensagem,
-        LocalDateTime dataCriacao,
-        StatusTopico status,
-        Long idAutor,
-        Long idCurso) {
-
-    public DadosDetalhamentoTopico(Topico topico) {
-        this(
-                topico.getId(),
-                topico.getTitulo(),
-                topico.getMensagem(),
-                topico.getDataCriacao(),
-                topico.getStatus(),
-                topico.getAutor().getId(),
-                topico.getCurso().getId()
-        );
-    }
-}
+//package hub.forum.api.dto.topico;
+//
+//import hub.forum.api.domain.topico.StatusTopico;
+//import hub.forum.api.domain.topico.Topico;
+//
+//import java.time.LocalDateTime;
+//
+//public record DadosDetalhamentoTopico(
+//        Long id,
+//        String titulo,
+//        String mensagem,
+//        LocalDateTime dataCriacao,
+//        StatusTopico status,
+//        Long idAutor,
+//        Long idCurso) {
+//
+//    public DadosDetalhamentoTopico(Topico topico) {
+//        this(
+//                topico.getId(),
+//                topico.getTitulo(),
+//                topico.getMensagem(),
+//                topico.getDataCriacao(),
+//                topico.getStatus(),
+//                topico.getAutor().getId(),
+//                topico.getCurso().getId()
+//        );
+//    }
+//}
