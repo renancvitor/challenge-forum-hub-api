e2c1d675ce3bce6680356c5ee31008b419c14610
renancvitor <renan.vitor.cm@gmail.com>
2025-06-18 07:33:29 -0300

    test(topico: adiciona testes unitários para listar topicos

---
+++ src/test/java/hub/forum/api/service/TopicoServiceTest.java
@@ -0,0 +1,18 @@
+package hub.forum.api.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class TopicoServiceTest {
+
+    @Test
+    void listar() {
+    }
+}
