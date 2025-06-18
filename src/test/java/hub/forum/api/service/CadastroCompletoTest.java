6b59bdcca5edecbcbb367c6297cf658684233b7a
renancvitor <renan.vitor.cm@gmail.com>
2025-06-18 14:45:36 -0300

    test(service): adiciona testes unitários para os métodos de cadastro de todas as classes de serviço

---
+++ src/test/java/hub/forum/api/service/CadastroCompletoTest.java
@@ -0,0 +1,78 @@
+package hub.forum.api.service;
+
+import hub.forum.api.domain.categoria.Categoria;
+import hub.forum.api.domain.perfil.Perfil;
+import hub.forum.api.domain.topico.StatusTopico;
+import hub.forum.api.domain.usuario.Usuario;
+import hub.forum.api.dto.curso.DadosCadastroCurso;
+import hub.forum.api.dto.perfil.DadosCadastroPerfil;
+import hub.forum.api.dto.resposta.DadosCadastroResposta;
+import hub.forum.api.dto.topico.DadosCadastroTopico;
+import hub.forum.api.dto.usuario.DadosCadastroUsuario;
+import hub.forum.api.repository.PerfilRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
+class CadastroCompletoTest {
+
+    @Autowired
+    CursoService cursoService;
+
+    @Autowired
+    PerfilService perfilService;
+
+    @Autowired
+    RespostaService respostaService;
+
+    @Autowired
+    TopicoService topicoService;
+
+    @MockBean
+    UsuarioLogadoService usuarioLogadoService;
+
+    @Autowired
+    UsuarioService usuarioService;
+
+    @Autowired
+    PerfilRepository perfilRepository;
+
+    @Test
+    void deveCadastrarTodosOsDadosParaTeste() {
+        var dadosPerfilFake = new DadosCadastroPerfil("TESTER-01");
+        perfilService.cadastrar(dadosPerfilFake);
+        var perfilFake = perfilRepository.findByNome("TESTER-01")
+                .orElseThrow(() -> new RuntimeException("Perfil não encontrado"));
+
+        var dadosUsuarioFake = new DadosCadastroUsuario("Fake", "fake@example.com",
+                "126456","TESTER-01");
+        var usuarioFake = usuarioService.cadastrar(dadosUsuarioFake);
+        Mockito.when(usuarioLogadoService.getUsuario())
+                .thenReturn(usuarioFake);
+
+        var dadosCurso = new DadosCadastroCurso("Java", Categoria.TECNOLOGIA);
+        var curso = cursoService.cadastrar(dadosCurso);
+
+        var dadosPerfil = new DadosCadastroPerfil("ADMIN-01");
+        var perfil = perfilService.cadastrar(dadosPerfil);
+
+        var dadosUsuario = new DadosCadastroUsuario("Renan", "renan@example.com", "123456",
+                "ADMIN-01");
+        var usuario = usuarioService.cadastrar(dadosUsuario);
+
+        var dadosTopico = new DadosCadastroTopico("Teste JUnit", "Testes automatizados Junit",
+                StatusTopico.NAO_RESPONDIDO, curso.nome());
+        var topico = topicoService.criar(dadosTopico);
+
+        var resposta = respostaService.cadastrar(topico.id(),
+                "Mensagem sobre o tópico");
+    }
+}
--- src/test/java/hub/forum/api/service/TopicoServiceTest.java
+++ src/test/java/hub/forum/api/service/TopicoServiceTest.java
@@ -1,6 +1,9 @@
 package hub.forum.api.service;
 
+import hub.forum.api.repository.TopicoRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -11,6 +14,13 @@ import static org.junit.jupiter.api.Assertions.*;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 class TopicoServiceTest {
+
+    @Mock
+    private TopicoRepository topicoRepository;
+
+    @InjectMocks
+    private TopicoService topicoService;
+
 
     @Test
     void listar() {
