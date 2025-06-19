ab23009040d817b5c0f9a672430e2caeb293f829
renancvitor <renan.vitor.cm@gmail.com>
2025-06-19 09:42:57 -0300

    refactor(repository): garante carregamento do perfil ao buscar usuário por email

--- src/main/java/hub/forum/api/repository/UsuarioRepository.java
+++ src/main/java/hub/forum/api/repository/UsuarioRepository.java
@@ -2,9 +2,12 @@ package hub.forum.api.repository;
 
 import hub.forum.api.domain.usuario.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Optional<Usuario> findByEmail(String email);
+    @Query("SELECT u FROM Usuario u JOIN FETCH u.perfil WHERE u.email = :email")
+    Optional<Usuario> findByEmail(@Param("email") String email);
 }
