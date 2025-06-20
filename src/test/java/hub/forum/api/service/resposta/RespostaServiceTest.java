6ba841c08075008a4f2459111fd704bb6996e8da
renancvitor <renan.vitor.cm@gmail.com>
2025-06-19 23:17:32 -0300

    test(resposta): adiciona testes unitários para o serviço atualizar

---
+++ src/test/java/hub/forum/api/service/resposta/RespostaServiceTest.java
@@ -0,0 +1,95 @@
+package hub.forum.api.service.resposta;
+
+import hub.forum.api.domain.categoria.Categoria;
+import hub.forum.api.domain.curso.Curso;
+import hub.forum.api.domain.perfil.Perfil;
+import hub.forum.api.domain.resposta.Resposta;
+import hub.forum.api.domain.topico.StatusTopico;
+import hub.forum.api.domain.topico.Topico;
+import hub.forum.api.domain.usuario.Usuario;
+import hub.forum.api.dto.resposta.DadosAtualizacaoResposta;
+import hub.forum.api.repository.*;
+import hub.forum.api.service.RespostaService;
+import hub.forum.api.service.TopicoService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
+class RespostaServiceTest {
+
+    @Autowired
+    TopicoRepository topicoRepository;
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @Autowired
+    CursoRepository cursoRepository;
+
+    @Autowired
+    PerfilRepository perfilRepository;
+
+    @Autowired
+    RespostaRepository respostaRepository;
+
+    @Autowired
+    RespostaService respostaService;
+
+    @Test
+    @Transactional
+    void atualizar() {
+        var perfil = perfilRepository.save(new Perfil("USUARIO"));
+
+        var usuario = new Usuario();
+        usuario.setNome("Usuario");
+        usuario.setEmail("usuario@example.com");
+        usuario.setSenha("123456");
+        usuario.setPerfil(perfil);
+        usuario = usuarioRepository.save(usuario);
+
+        var curso = new Curso();
+        curso.setNome("Spring Boot 3.0 atualiza");
+        curso.setCategoria(Categoria.TECNOLOGIA);
+        curso = cursoRepository.save(curso);
+
+        var topico = new Topico();
+        topico.setTitulo("Teste atualiza");
+        topico.setMensagem("Mensagem atualiza");
+        topico.setDataCriacao(LocalDateTime.now());
+        topico.setStatus(StatusTopico.NAO_RESPONDIDO);
+        topico.setAutor(usuario);
+        topico.setCurso(curso);
+        topico.setAtivo(true);
+        topicoRepository.save(topico);
+
+        var resposta = new Resposta();
+        resposta.setTopico(topico);
+        resposta.setMensagem("Mensagem atualiza");
+        resposta.setDataCriacao(LocalDateTime.now());
+        resposta.setAutor(usuario);
+        resposta.setAtivo(true);
+        resposta.setSolucao(false);
+        respostaRepository.save(resposta);
+
+        var dadosAtualiza = new DadosAtualizacaoResposta("Mensagem atualizada");
+        var resultado = respostaService.atualizar(
+                resposta.getId(),
+                dadosAtualiza,
+                usuario
+        );
+
+        assertNotNull(resultado);
+        assertEquals("Mensagem atualizada", resultado.mensagem());
+    }
+}
