bfef5ecfec2c255234cb0a37885bbaf0e9850fd4
renancvitor <renan.vitor.cm@gmail.com>
2025-06-23 10:12:01 -0300

    test(controller): adiciona teste unitário para listar topico - status OK

--- src/test/java/hub/forum/api/controller/topico/TopicoControllerAtualizarTest.java
+++ src/test/java/hub/forum/api/controller/topico/TopicoControllerAtualizarTest.java
@@ -1,10 +1,9 @@
-package hub.forum.api.controller;
+package hub.forum.api.controller.topico;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import hub.forum.api.domain.perfil.Perfil;
 import hub.forum.api.domain.usuario.Usuario;
-import hub.forum.api.dto.topico.DadosListagemTotalTopico;
+import hub.forum.api.dto.topico.DadosAtualizacaoTopico;
+import hub.forum.api.dto.topico.DadosDetalhamentoResumidoTopico;
 import hub.forum.api.service.TopicoService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,9 +14,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
@@ -27,34 +23,33 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureJsonTesters
-class TopicoControllerTest {
+class TopicoControllerAtualizarTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private JacksonTester<DadosAtualizacaoTopico> dadosAtualizacaoTopicoJacksonTester;
+
+    @Autowired
+    private JacksonTester<DadosDetalhamentoResumidoTopico> dadosDetalhamentoResumidoTopicoJacksonTester;
+
     @MockBean
     private TopicoService topicoService;
-
-    @Autowired
-    private JacksonTester<DadosListagemTotalTopico> dadosListagemTotalTopicoJacksonTester;
-
-    @Autowired
-    private ObjectMapper objectMapper;
 
     @Test
     @DisplayName("Cadastro de perfil: deveria devolver 200")
     @WithMockUser(username = "renan", roles = {"ADMIN"})
-    void listar() throws Exception {
+    void atualizar() throws Exception {
         Usuario usuarioLogado = new Usuario();
         usuarioLogado.setPerfil(new Perfil("ADMIN"));
 
@@ -66,32 +61,32 @@ class TopicoControllerTest {
 
         SecurityContextHolder.setContext(securityContext);
 
-        var dadosListagemTotalTopico = new DadosListagemTotalTopico(
+        var dadosDetalhamentoTopico = new DadosDetalhamentoResumidoTopico(
                 null,
                 "Título",
                 "Mensagem",
                 LocalDateTime.now());
 
-        Page<DadosListagemTotalTopico> paginaMockada =
-                new PageImpl<>(List.of(dadosListagemTotalTopico), PageRequest.of(0, 10), 1);
-
-        when(topicoService.listar(any())).thenReturn(paginaMockada);
+        when(topicoService.atualizar(any(), any(), any())).thenReturn(dadosDetalhamentoTopico);
 
         var response = mockMvc
-                .perform(get("/topicos")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andReturn()
-                .getResponse();
+                .perform(
+                        put("/topicos/1")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(dadosAtualizacaoTopicoJacksonTester.write(
+                                        new DadosAtualizacaoTopico(
+                                                "Titulo att",
+                                                "Mensagem att"
+                                        )
+                                ).getJson())
+                ).andReturn().getResponse();
 
         assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
 
-        JsonNode jsonNode = objectMapper.readTree(response.getContentAsString());
+        var jsonEsperado = dadosDetalhamentoResumidoTopicoJacksonTester.write(
+                dadosDetalhamentoTopico
+        ).getJson();
 
-        assertThat(jsonNode.get("content")).isNotNull();
-        assertThat(jsonNode.get("content").isArray()).isTrue();
-        assertThat(jsonNode.get("content").size()).isEqualTo(1);
-
-        JsonNode primeiroTopico = jsonNode.get("content").get(0);
-        assertThat(primeiroTopico.get("titulo").asText()).isEqualTo("Título");
+        assertThat(response.getContentAsString()).isEqualTo(jsonEsperado);
     }
 }
