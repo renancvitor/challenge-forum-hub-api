68bb397911b76d1b34d07878b5333e827e5c2752
renancvitor <renan.vitor.cm@gmail.com>
2025-06-23 22:58:22 -0300

    test(controller): adiciona teste unitário para marcar solução topico - status BAD REQUEST

---
+++ src/test/java/hub/forum/api/controller/TopicoControllerMarcarSolucaoTestERRO.java
@@ -0,0 +1,62 @@
+package hub.forum.api.controller;
+
+import hub.forum.api.domain.perfil.Perfil;
+import hub.forum.api.domain.usuario.Usuario;
+import hub.forum.api.infra.exception.ValidacaoException;
+import hub.forum.api.service.RespostaService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class TopicoControllerMarcarSolucaoTestERRO {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private RespostaService respostaService;
+
+    @Test
+    @DisplayName("Deveria retornar 400 quando usuário não for o autor do tópico")
+    void deveRetornar400QuandoUsuarioNaoEhAutor() throws Exception {
+        Usuario usuarioLogado = new Usuario();
+        usuarioLogado.setId(1L);
+        usuarioLogado.setNome("Renan");
+        usuarioLogado.setEmail("renan@example.com");
+        usuarioLogado.setSenha("123456");
+        usuarioLogado.setPerfil(new Perfil("ADMIN"));
+
+        var auth = new UsernamePasswordAuthenticationToken(
+                usuarioLogado,
+                null,
+                List.of(new SimpleGrantedAuthority("ROLE_ADMIN"))
+        );
+
+        doThrow(new ValidacaoException("Apenas o autor do tópico pode marcar uma resposta como solução"))
+                .when(respostaService)
+                .marcarSolucao(6L, 3L, 1L);
+
+        mockMvc.perform(put("/topicos/3/resposta/6/solucao")
+                        .with(SecurityMockMvcRequestPostProcessors.authentication(auth)))
+                .andExpect(status().isBadRequest());
+
+        verify(respostaService).marcarSolucao(6L, 3L, 1L);
+    }
+}
